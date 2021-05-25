7bdbba2d8ca07d3cbc97f894fcc01a19f29741a4
James <Thylinguistics@gmail.com>
2021-05-25 14:30:59 -0700

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
-
+import { AuthContext } from "../contexts/AuthContext";
 class Navbar extends Component {
   //static contextType = ThemeContext;
 
@@ -9,25 +9,29 @@ class Navbar extends Component {
     // const { isLightTheme, light, dark } = this.context;
     // const theme = isLightTheme ? light : dark;
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
 
-          return (
-            <>
-              <nav style={{ background: theme.ui, color: theme.color }}>
-                <h1>Context App</h1>
-                <ul>
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>Contact</li>
-                </ul>
-              </nav>
-            </>
-          );
-        }}
-      </ThemeContext.Consumer>
+              return (
+                <>
+                  <nav style={{ background: theme.ui, color: theme.color }}>
+                    <h1>Context App</h1>
+                    <ul>
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>Contact</li>
+                    </ul>
+                  </nav>
+                </>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
